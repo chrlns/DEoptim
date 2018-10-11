45800756000c87c1d747f3ac436c13cf45080701
Christian Lins <christian@lins.me>
2018-10-11 07:02:16 +0200

    DEopencl: Rename DEopencl.cpp to main.cpp

--- DEopencl/oclPlatform.cpp
+++ DEopencl/oclPlatform.cpp
@@ -36,6 +36,54 @@ shared_ptr<oclPlatform> oclPlatform::queryDefaultPlatform() {
 }
 
 
-oclPlatform::oclPlatform(int id) {
+oclPlatform::oclPlatform(cl_platform_id id) : id(id) {
 
 }
+
+void oclPlatform::printDeviceInfo(cl_device_id id) {
+	char* buf = new char[512];
+	cl_uint buf_uint;
+	cl_ulong buf_ulong;
+	size_t bufSize;
+
+	clGetDeviceInfo(id, CL_DEVICE_NAME, 512, buf, &bufSize);
+	cout << "\tDevice " << id << " " << (char*)buf << endl;
+
+	clGetDeviceInfo(id, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(cl_uint), &buf_uint, &bufSize);
+	cout << "\t\tMax. compute units = " << buf_uint << endl;
+
+	clGetDeviceInfo(id, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(cl_ulong), &buf_ulong, &bufSize);
+	cout << "\t\tMax. global mem size = " << buf_ulong / 1024 / 1024 << " MiB" << endl;
+}
+
+void oclPlatform::printPlatformInfo() {
+    #define BUF_LIMIT 512
+	char* buf = new char[BUF_LIMIT];
+	size_t bufSize;
+
+	clGetPlatformInfo(id, CL_PLATFORM_VERSION, BUF_LIMIT, buf, &bufSize);
+	cout << id << ": " << (char*)buf;
+
+	clGetPlatformInfo(id, CL_PLATFORM_NAME, BUF_LIMIT, buf, &bufSize);
+	cout << " " << (char*)buf << endl;
+
+	clGetPlatformInfo(id, CL_PLATFORM_VENDOR, BUF_LIMIT, buf, &bufSize);
+	cout << "Vendor: " << (char*)buf << endl;
+
+	clGetPlatformInfo(id, CL_PLATFORM_EXTENSIONS, BUF_LIMIT, buf, &bufSize);
+	cout << "Extensions: " << (char*)buf << endl;
+
+    // Device information
+	cl_device_id devices[8];
+	cl_uint numDevices;
+	if (clGetDeviceIDs(id, CL_DEVICE_TYPE_ALL, 8, devices, &numDevices) != CL_SUCCESS) {
+		cerr << "Error clGetDeviceIDs" << endl;
+	} else {
+		for (cl_uint n = 0; n < numDevices; n++) {
+			printDeviceInfo(devices[n]);
+		}
+	}
+
+	cout << endl;
+}
+
--- DEopencl/oclPlatform.h
+++ DEopencl/oclPlatform.h
@@ -3,14 +3,22 @@
 #include <memory>
 #include <vector>
 
+#define CL_TARGET_OPENCL_VERSION 120
 #include <CL/cl.h>
 
 using namespace std;
 
 class oclPlatform {
+    private:
+        cl_platform_id id;
+
     public:
         static vector<shared_ptr<oclPlatform>> queryPlatforms();
         static shared_ptr<oclPlatform> queryDefaultPlatform();
 
-        oclPlatform(int id);
+        oclPlatform(cl_platform_id id);
+
+        
+        void printDeviceInfo(cl_device_id);
+        void printPlatformInfo();
 };
